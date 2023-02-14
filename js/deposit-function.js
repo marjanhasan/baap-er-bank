ea22ea8ef3192f90508079afea2ad56991ad0763
marjanhasan <moonmarjanhasan@gmail.com>
2023-02-14 22:01:03 +0600

    three more js file added to operate every operation using function to reduce repetation of code!

--- js/deposit-function.js
+++ js/deposit-function.js
@@ -1,1 +1,7 @@
-console.log('deposit')
+document.getElementById('btn-deposit').addEventListener('click',function(){
+    const enteredAmount = getInputValue('deposit-field');
+    const previousDepositValue = getValueFromText('deposit-total');
+    const newDepositValue = enteredAmount + previousDepositValue;
+    setValueAsText('deposit-total', newDepositValue);
+    
+});
--- js/utilities.js
+++ js/utilities.js
@@ -1,1 +1,17 @@
-console.log('utilities')
+function getInputValue(id){
+    const inputField = document.getElementById(id);
+    const inputFieldStringValue = inputField.value;
+    const inputFieldValue = parseFloat(inputFieldStringValue);
+    inputField.value = '';
+    return inputFieldValue;
+}
+
+function getValueFromText(id){
+    const valueFromText = parseFloat(document.getElementById(id).innerText);
+    return valueFromText;
+}
+
+function setValueAsText(id, value){
+    const valueFromText = document.getElementById(id);
+    const finalValue = valueFromText.innerHTML = value;
+}

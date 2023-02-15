1ab1d7eac75a682680a0c62c560b8293e813e7d8
marjanhasan <moonmarjanhasan@gmail.com>
2023-02-15 15:33:36 +0600

    withdraw & deposit operation succesfully done by js

--- js/deposit-function.js
+++ js/deposit-function.js
@@ -4,4 +4,7 @@ document.getElementById('btn-deposit').addEventListener('click',function(){
     const newDepositValue = enteredAmount + previousDepositValue;
     setValueAsText('deposit-total', newDepositValue);
     
+    const previousBalanceValue = getValueFromText('balance-total');
+    const newBalanceValue = enteredAmount + previousBalanceValue;
+    setValueAsText('balance-total', newBalanceValue);
 });
--- js/withdraw-function.js
+++ js/withdraw-function.js
@@ -1,1 +1,10 @@
-console.log('withdraw')
+document.getElementById('btn-withdraw').addEventListener('click',function(){
+    const enteredAmount = getInputValue('withdraw-field');
+    const previousDepositValue = getValueFromText('withdraw-total');
+    const newDepositValue = enteredAmount + previousDepositValue;
+    setValueAsText('withdraw-total', newDepositValue);
+    
+    const previousBalanceValue = getValueFromText('balance-total');
+    const newBalanceValue =previousBalanceValue - enteredAmount;
+    setValueAsText('balance-total', newBalanceValue);
+});
